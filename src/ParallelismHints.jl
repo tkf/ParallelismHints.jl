d74fe51d9ac1ba424eb0cdead9c0f85e56cc0fe1
Takafumi Arakaki <aka.tkf@gmail.com>
2022-04-12 04:51:15 -0400

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.26

---
+++ src/ParallelismHints.jl
@@ -0,0 +1,11 @@
+baremodule ParallelismHints
+
+module Internal
+
+using ..ParallelismHints: ParallelismHints
+
+include("internal.jl")
+
+end  # module Internal
+
+end  # baremodule ParallelismHints
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using ParallelismHints
+using Test
+
+@testset "ParallelismHints.jl" begin
+    # Write your tests here.
+end
